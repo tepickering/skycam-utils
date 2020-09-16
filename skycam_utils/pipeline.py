fix up stellacam pipeline to apply masks to outputs and handle masking properly in background subtraction

skycam_utils/pipeline.py
@@ -4,8 +4,10 @@ from pathlib import Path
 
 import numpy as np
 
+import astropy
 import astropy.units as u
 import astropy.wcs as wcs
+from astropy.io import fits
 from astropy.nddata import CCDData
 
 from .photometry import make_background, make_segmentation_image, make_catalog
@@ -60,7 +62,7 @@ def process_asi_image(fitsfile):
     return s_cat, p_cat
 
 
-def process_stellacam_image(fitsfile, wcs=None, mask=None, write=False, zp=-9.):
+def process_stellacam_image(fitsfile, wcs=None, mask=None, write=False, zp=0.):
     """
     Process a FITS image from the stellacam all-sky camera to extract the background, perform photometry
     of the sources  detected in the image, and, if provided a wcs, measure a photometric zeropoint using
@@ -79,21 +81,24 @@ def process_stellacam_image(fitsfile, wcs=None, mask=None, write=False, zp=-9.):
         If True, write outputs to FITS files.
 
     zp : float
-        Photometric zeropoint. Assume -9 as reasonable default given initial analysis.
+        Photometric zeropoint. Default to 0 for raw instrumental magnitudes.
     """
     if isinstance(fitsfile, str):
         fitsfile = Path(fitsfile)
-    im = CCDData.read(fitsfile, unit=u.adu)
 
-    if mask is not None:
-        im = np.ma.masked_where(mask == np.nan, im)
-    bkg = make_background(im, boxsize=(5, 5), filter_size=(3, 3))
+    with fits.open(fitsfile) as hdul:
+        im = hdul[0].data
 
-    bkg_image = CCDData(bkg.background, unit=u.adu)
+    if mask is None:
+        mask = np.ones_like(im)
+
+    bkg = make_background(im, boxsize=(5, 5), filter_size=(3, 3), inmask=mask)
+
+    bkg_image = CCDData(bkg.background * mask, unit=u.adu)
     if write:
         bkg_image.write(fitsfile.with_suffix(".bkg.fits"), overwrite=True)
 
-    diff = CCDData(im.data - bkg_image.data, unit=u.adu)
+    diff = CCDData(im.data * mask - bkg_image.data, unit=u.adu)
     if write:
         diff_fp = fitsfile.with_suffix(".subt.fits")
         diff.write(diff_fp, overwrite=True)
@@ -102,10 +107,10 @@ def process_stellacam_image(fitsfile, wcs=None, mask=None, write=False, zp=-9.):
 
     catalog = make_catalog(im, segm)
     if write:
-        catalog.write(fitsfile.with_suffix(".cat.fits"), overwrite=True)
+        catalog.write(fitsfile.with_suffix(".cat.csv"), overwrite=True)
 
     if wcs is not None:
-        pix_scales = wcs.utils.proj_plane_pixel_scales(wcs)
+        pix_scales = astropy.wcs.utils.proj_plane_pixel_scales(wcs)
         pix_area = pix_scales[0] * pix_scales[1] * 3600.**2
     else:
         # stellacam pixel scale is about 0.27 deg/pixel
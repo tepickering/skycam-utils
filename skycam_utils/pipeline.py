6e650f4684b5f46b3966d661c3109a600ee03e2e
T. E. Pickering <te.pickering@gmail.com>
2020-09-17 21:44:19 -0700

    set up function to run as an entry_point for processing directories from the command line. use Pool to parallelize the process

--- skycam_utils/pipeline.py
+++ skycam_utils/pipeline.py
@@ -1,7 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import datetime
+import argparse
+import multiprocessing
+import warnings
+
 from pathlib import Path
+from functools import partial
 
 import numpy as np
 
@@ -10,9 +15,13 @@ import astropy.units as u
 import astropy.wcs as wcs
 from astropy.io import fits
 from astropy.nddata import CCDData
+from astropy.time import Time
 
-from .photometry import make_background, make_segmentation_image, make_catalog
-from .astrometry import solve_field
+from .photometry import make_background, make_segmentation_image, make_catalog, load_mask, match_stars, load_skycam_catalog
+from .astrometry import solve_field, load_wcs, update_altaz
+
+
+warnings.filterwarnings('ignore')
 
 
 def get_ut(hdr, year=2020):
@@ -76,7 +85,7 @@ def process_asi_image(fitsfile):
     return s_cat, p_cat
 
 
-def process_stellacam_image(fitsfile, wcs=None, mask=None, write=False, zp=0.):
+def process_stellacam_image(fitsfile, year, write=False, zp=0., return_products=False):
     """
     Process a FITS image from the stellacam all-sky camera to extract the background, perform photometry
     of the sources  detected in the image, and, if provided a wcs, measure a photometric zeropoint using
@@ -85,14 +94,11 @@ def process_stellacam_image(fitsfile, wcs=None, mask=None, write=False, zp=0.):
     fitsfile : str or `~pathlib.PosixPath`
         FITS file to process.
 
-    wcs : `~astropy.wcs.WCS` object
-        WCS to map (x,y) to (az, alt)
-
-    mask : ndarray-like
-        Mask image to define regions outside of the FOV or obscured by the MMT or flagpole.
+    year : int
+        Required to set which WCS and field mask to use.
 
     write : bool
-        If True, write outputs to FITS files.
+        If True, write intermediate FITS outputs. Matched catalog is always written.
 
     zp : float
         Photometric zeropoint. Default to 0 for raw instrumental magnitudes.
@@ -102,9 +108,20 @@ def process_stellacam_image(fitsfile, wcs=None, mask=None, write=False, zp=0.):
 
     with fits.open(fitsfile) as hdul:
         im = hdul[0].data
+        hdr = hdul[0].header
 
-    if mask is None:
-        mask = np.ones_like(im)
+    # we only do the full photometric analysis when the camera is in the dark sky steady state
+    # configuration of 256 frames integration with a gain of 106.
+    if hdr['FRAME'] != '256 Frames' or hdr['GAIN'] != 106:
+        print(f"Not processing {fitsfile} with frame={hdr['FRAME']} and gain={hdr['GAIN']}...")
+        return None
+    else:
+        print(f"Processing {fitsfile}...")
+
+    mask = load_mask(year=year)
+    wcs = load_wcs(year=year)
+    tobs = get_ut(hdr, year=year)
+    skycat = update_altaz(load_skycam_catalog(), time=tobs)
 
     bkg = make_background(im, boxsize=(5, 5), filter_size=(3, 3), inmask=mask)
 
@@ -120,8 +137,11 @@ def process_stellacam_image(fitsfile, wcs=None, mask=None, write=False, zp=0.):
     segm = make_segmentation_image(diff.data)
 
     catalog = make_catalog(im, segm, background=bkg)
-    if write:
-        catalog.write(fitsfile.with_suffix(".cat.csv"), overwrite=True)
+    matched = match_stars(skycat, catalog, wcs, max_sep=2.5*u.deg)
+    try:
+        matched.write(fitsfile.with_suffix(".cat.csv"), overwrite=True)
+    except:
+        pass
 
     if wcs is not None:
         pix_scales = astropy.wcs.utils.proj_plane_pixel_scales(wcs)
@@ -135,4 +155,53 @@ def process_stellacam_image(fitsfile, wcs=None, mask=None, write=False, zp=0.):
     if write:
         sky_mag.write(fitsfile.with_suffix(".sky.fits"), overwrite=True)
 
-    return bkg_image, diff, segm, sky_mag, catalog
+    if return_products:
+        return bkg_image, diff, segm, sky_mag, matched
+    else:
+        return None
+
+
+def process_stellacam_dir():
+    """
+    Work through and process directory of stellacam allsky camera images. This function is meant to be the core of
+    a command-line script.
+    """
+    parser = argparse.ArgumentParser(description="Script for doing photometric processing of a directory of stellacam images.")
+
+    parser.add_argument(
+        'rootdir',
+        metavar="<directory of allsky images>",
+        help="Directory containing stellecam allsky camera images. Directory name expected to be of format YYYYMMDD."
+    )
+
+    parser.add_argument(
+        '--writefits',
+        help="Write intermediate images out as FITS files.",
+        action="store_true"
+    )
+
+    parser.add_argument(
+        '--zeropoint',
+        help="Photometric zeropoint to apply to source magnitudes. (default: 0.0)",
+        default=0.0
+    )
+
+    parser.add_argument(
+        '--nproc',
+        help="Number of processors to utilize. (default: 6)",
+        default=6
+    )
+
+    args = parser.parse_args()
+
+    rootdir = Path(args.rootdir)
+    year = int(rootdir.name[0:4])
+    files = sorted(list(rootdir.glob("*.fits")))
+
+    process = partial(process_stellacam_image, year=year, write=args.writefits, zp=args.zeropoint)
+    with multiprocessing.Pool(processes=args.nproc) as pool:
+        pool.map(process, files)
+
+
+if __name__ == "__main__":
+    process_stellacam_dir()

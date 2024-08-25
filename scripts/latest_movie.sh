c7e137ed30fdb05e071374c6ab8e1b93810660cb
T. E. Pickering <te.pickering@gmail.com>
2024-08-25 10:45:02 -0700

    switch movie length from 100 to 150 frames

--- scripts/latest_movie.sh
+++ scripts/latest_movie.sh
@@ -2,7 +2,7 @@
 
 cur=`ls -d /mnt/d/skycam/20* | tail -1`
 
-for i in `ls $cur/20*.jpg | tail -100`; do echo "file '$i'"; done > /mnt/d/skycam/ff_input.txt
+for i in `ls $cur/20*.jpg | tail -150`; do echo "file '$i'"; done > /mnt/d/skycam/ff_input.txt
 
 ffmpeg -f concat -safe 0 -i /mnt/d/skycam/ff_input.txt -vcodec mpeg4 /var/www/html/skycam/tmp.mp4
 

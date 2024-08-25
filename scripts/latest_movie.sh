0ce3e0b62cb0396902ae4dc6b279c27d6ec94224
T. E. Pickering <te.pickering@gmail.com>
2024-08-25 00:46:43 -0700

    add script for generating movie of last 100 skycam images

---
+++ scripts/latest_movie.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cur=`ls -d /mnt/d/skycam/20* | tail -1`
+
+for i in `ls $cur/20*.jpg | tail -100`; do echo "file '$i'"; done > /mnt/d/skycam/ff_input.txt
+
+ffmpeg -f concat -safe 0 -i /mnt/d/skycam/ff_input.txt -vcodec mpeg4 /var/www/html/skycam/tmp.mp4
+
+mv /var/www/html/skycam/tmp.mp4 /var/www/html/skycam/latest_movie.mp4
+

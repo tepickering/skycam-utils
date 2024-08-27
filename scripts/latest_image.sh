927510cef35041e89b962035b551f0a7b37dcff0
T. E. Pickering <te.pickering@gmail.com>
2024-08-26 19:24:29 -0700

    fix path in latest_image

--- scripts/latest_image.sh
+++ scripts/latest_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-scp /var/www/html/ImageLastFTP_AllSKY.jpg tim@ops.mmto.arizona.edu:/var/www/html/new_skycam/latest_image.jpg
+scp /var/www/html/skycam/ImageLastFTP_AllSKY.jpg tim@ops.mmto.arizona.edu:/var/www/html/new_skycam/latest_image.jpg
 
 sleep 30
 
-scp /var/www/html/ImageLastFTP_AllSKY.jpg tim@ops.mmto.arizona.edu:/var/www/html/new_skycam/latest_image.jpg
+scp /var/www/html/skycam/ImageLastFTP_AllSKY.jpg tim@ops.mmto.arizona.edu:/var/www/html/new_skycam/latest_image.jpg

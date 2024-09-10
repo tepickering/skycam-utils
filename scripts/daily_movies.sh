4fcaaf571dd4de8fe2f5a4ad656ad747409dca39
T. E. Pickering <te.pickering@gmail.com>
2024-09-09 18:03:39 -0700

    add scripts for making daily movies

---
+++ scripts/daily_movies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cur = ls -d /mnt/d/skycam/20* | tail -2 | head -1 | cut -d'/' -f5
+
+/home/skycam/skycam-utils/scripts/make_movies.sh $cur
---
+++ scripts/mmt_position.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import json
+import os
+import redis
+
+from astropy.time import Time
+from astropy.coordinates import Angle, EarthLocation
+import astropy.units as u
+
+
+if 'REDISHOST' in os.environ:
+    redis_host = os.environ['REDISHOST']
+else:
+    redis_host = 'redis.mmto.arizona.edu'
+
+if 'REDISPORT' in os.environ:
+    redis_port = os.environ['REDISPORT']
+else:
+    redis_port = 6379
+
+if 'REDISPW' in os.environ:
+    redis_server = redis.StrictRedis(
+        host=redis_host,
+        port=redis_port,
+        password=os.environ['REDISPW'],
+        db=0,
+        decode_responses=True
+    )
+else:
+    redis_server = redis.StrictRedis(host=redis_host, port=redis_port, db=0, decode_responses=True)
+
+mmt = EarthLocation.from_geodetic("-110:53:04.4", "31:41:19.6", 2600 * u.m)
+
+mmt_lat = mmt.lat.to(u.radian).value
+
+now = Time(Time.now(), location=mmt)
+
+# alcor uses the dublin JD, which is weird, but needs a 1.5 day offset to match example in the docs
+now_alcor = now.jd - 2415020 + 1.5
+
+mmt_ra = Angle(json.loads(redis_server.get('mount_mini_ra'))['value'], u.hourangle).to(u.radian).value
+mmt_dec = Angle(json.loads(redis_server.get('mount_mini_declination'))['value'], u.deg).to(u.radian).value
+mmt_lst = now.sidereal_time('apparent').to(u.radian).value
+mmt_catra = Angle(float(json.loads(redis_server.get('mount_mini_cat_ra2000'))['value']) * u.hourangle).to(u.radian).value
+mmt_catdec = Angle(float(json.loads(redis_server.get('mount_mini_cat_dec2000'))['value']) * u.deg).to(u.radian).value
+
+if mmt_catra < 0:
+    mmt_ra = mmt_lst
+    mmt_dec = mmt_lat
+
+print(f"mmt_ra: {mmt_ra}, mmt_dec: {mmt_dec}")

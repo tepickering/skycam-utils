351b0bdbb4182f9747fe38e6fd15db37adab8e27
T. E. Pickering <te.pickering@gmail.com>
2024-09-09 18:25:44 -0700

    up bitrate for unwrap movie

--- scripts/make_movies.sh
+++ scripts/make_movies.sh
@@ -8,4 +8,4 @@ for i in `ls /mnt/d/skycam/$datedir/20*.jpg`; do echo "file '$i'"; done > /mnt/d
 for i in `ls /mnt/d/skycam/$datedir/Unwrap*.jpg`; do echo "file '$i'"; done > /mnt/d/skycam/unwrap_input.txt
 
 ffmpeg -f concat -safe 0 -i /mnt/d/skycam/movie_input.txt -c:v h264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -b:v 6000k /mnt/d/skycam/movies/$datedir/allsky.mp4
-ffmpeg -f concat -safe 0 -i /mnt/d/skycam/unwrap_input.txt -c:v h264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -b:v 600k /mnt/d/skycam/movies/$datedir/unwrap.mp4
+ffmpeg -f concat -safe 0 -i /mnt/d/skycam/unwrap_input.txt -c:v h264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -b:v 1200k /mnt/d/skycam/movies/$datedir/unwrap.mp4

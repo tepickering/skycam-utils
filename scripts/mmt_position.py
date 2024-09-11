c3fb65542fa765ce1e99f9caa4083dbb7098fbf1
T. E. Pickering <te.pickering@gmail.com>
2024-09-11 11:01:38 -0700

    working version ready to test for mmt position

--- scripts/mmt_position.py
+++ scripts/mmt_position.py
@@ -2,6 +2,8 @@
 
 import json
 import os
+import time
+
 import redis
 
 from astropy.time import Time
@@ -34,20 +36,23 @@ mmt = EarthLocation.from_geodetic("-110:53:04.4", "31:41:19.6", 2600 * u.m)
 
 mmt_lat = mmt.lat.to(u.radian).value
 
-now = Time(Time.now(), location=mmt)
+while True:
+    now = Time(Time.now(), location=mmt)
 
-# alcor uses the dublin JD, which is weird, but needs a 1.5 day offset to match example in the docs
-now_alcor = now.jd - 2415020 + 1.5
+    # alcor uses the dublin JD, which is weird, but needs a 1.5 day offset to match example in the docs
+    now_alcor = now.jd - 2415020 + 1.5
 
-mmt_ra = Angle(json.loads(redis_server.get('mount_mini_ra'))['value'], u.hourangle).to(u.radian).value
-mmt_dec = Angle(json.loads(redis_server.get('mount_mini_declination'))['value'], u.deg).to(u.radian).value
-mmt_lst = now.sidereal_time('apparent').to(u.radian).value
-mmt_catra = Angle(float(json.loads(redis_server.get('mount_mini_cat_ra2000'))['value']) * u.hourangle).to(u.radian).value
-mmt_catdec = Angle(float(json.loads(redis_server.get('mount_mini_cat_dec2000'))['value']) * u.deg).to(u.radian).value
+    mmt_ra = Angle(json.loads(redis_server.get('mount_mini_ra'))['value'], u.hourangle).to(u.radian).value
+    mmt_dec = Angle(json.loads(redis_server.get('mount_mini_declination'))['value'], u.deg).to(u.radian).value
+    mmt_lst = now.sidereal_time('apparent').to(u.radian).value
+    mmt_catra = Angle(float(json.loads(redis_server.get('mount_mini_cat_ra2000'))['value']) * u.hourangle).to(u.radian).value
+    mmt_catdec = Angle(float(json.loads(redis_server.get('mount_mini_cat_dec2000'))['value']) * u.deg).to(u.radian).value
 
-if mmt_catra < 0:
-    mmt_ra = mmt_lst
-    mmt_dec = mmt_lat
+    if mmt_catra < 0:
+        mmt_ra = mmt_lst
+        mmt_dec = mmt_lat
 
-with open("position.txt", "w") as f:
-    f.write(f"{now_alcor}\n{mmt_ra}\n{mmt_dec}\n")
+    with open("/mnt/d/skycam/mmt_position.txt", "w") as f:
+        f.write(f"{now_alcor}\n{mmt_ra}\n{mmt_dec}\n")
+
+    time.sleep(5)
